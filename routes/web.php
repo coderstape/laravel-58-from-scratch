cb6f3f2ecce662182946f282f70ce9aa1075add7
Victor Gonzalez <vicgonvt@gmail.com>
2019-03-08 19:34:06 -0500

    e22 - Middleware

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::view('about', 'about');
 //Route::delete('customers/{customer}', 'CustomersController@destroy');
 
 Route::resource('customers', 'CustomersController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
