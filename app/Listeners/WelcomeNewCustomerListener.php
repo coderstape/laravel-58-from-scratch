b57fda072ecad22acf58b3b18e55793ea23fddcf
Victor Gonzalez <vicgonvt@gmail.com>
2019-03-19 19:57:43 -0400

    e29 - Queues

--- app/Listeners/WelcomeNewCustomerListener.php
+++ app/Listeners/WelcomeNewCustomerListener.php
@@ -3,9 +3,10 @@
 namespace App\Listeners;
 
 use App\Mail\WelcomeNewUserMail;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 
-class WelcomeNewCustomerListener
+class WelcomeNewCustomerListener implements ShouldQueue
 {
     /**
      * Handle the event.
@@ -15,6 +16,8 @@ class WelcomeNewCustomerListener
      */
     public function handle($event)
     {
+        sleep(10);
+
         Mail::to($event->customer->email)->send(new WelcomeNewUserMail());
     }
 }
