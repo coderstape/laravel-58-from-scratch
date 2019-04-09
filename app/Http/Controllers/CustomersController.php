dbd208e11a0c5818ffe9a244d6d5684639c49618
Victor Gonzalez <vicgonvt@gmail.com>
2019-04-08 22:10:35 -0400

    e40 - Image resize

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -8,6 +8,7 @@ use App\Events\NewCustomerHasRegisteredEvent;
 use App\Mail\WelcomeNewUserMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Intervention\Image\Facades\Image;
 
 class CustomersController extends Controller
 {
@@ -72,21 +73,13 @@ class CustomersController extends Controller
 
     private function validateRequest()
     {
-        return tap(request()->validate([
+        return request()->validate([
             'name' => 'required|min:3',
             'email' => 'required|email',
             'active' => 'required',
             'company_id' => 'required',
-
-        ]), function () {
-
-            if (request()->hasFile('image')) {
-                request()->validate([
-                    'image' => 'file|image|max:5000',
-                ]);
-            }
-
-        });
+            'image' => 'sometimes|file|image|max:5000',
+        ]);
     }
 
     private function storeImage($customer)
@@ -95,6 +88,9 @@ class CustomersController extends Controller
             $customer->update([
                 'image' => request()->image->store('uploads', 'public'),
             ]);
+
+            $image = Image::make(public_path('storage/' . $customer->image))->fit(300, 300, null, 'top-left');
+            $image->save();
         }
     }
 }
